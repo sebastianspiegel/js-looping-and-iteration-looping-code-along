50443befe60a728d8e855c461646dd73d55b32f4
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-26 09:55:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(num) {
+    while(num > -1) {
+        console.log(num--);
+    }
+}
+
+function writeCards(names, events) {
+    let array = []
+    for(let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return array; 
+}
